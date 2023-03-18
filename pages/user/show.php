c9ecfb7b7e8efaa7dc289152f0ac634dd3cc0b66
thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-03-18 18:34:52 +0700

    Update show.php

---
+++ pages/user/show.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<script src="https://kit.fontawesome.com/11a9c95312.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Home Page</title>
+</head>
+<body>
+    <section id="section1">
+        <div class="section1-content">
+            <div class="section1-content-left">
+                <h1>WELLCOME TO SUPER</h1>
+                <h3>Now you can go anywhere with our car booking service.</h3>
+            </div>
+            <div class="section1-content-right">
+                <form class="section1-form">
+                    <span>PICK UP LOCATION</span>
+                    <select id="from-pickUp-location">
+                        <option value="Đà Nẵng">Đà Nẵng</option>
+                        <option value="Hà Nội">Hà Nội</option>
+                        <option value="Hồ Chí Minh">Hồ Chí Minh</option>
+                        <option value="Huế">Huế</option>
+                        <option value="Nha Trang">Nha Trang</option>
+                        <option value="Hải Dương">Hải Dương</option>
+                        <option value="Quảng Bình">Quảng Bình</option>
+                    </select>
+                    <span><i class="fa-sharp fa-solid fa-location-dot"></i></span>
+                    <br>
+                    <select id="from-drop-location">
+                        <option value="Đà Nẵng">Đà Nẵng</option>
+                        <option value="Hà Nội">Hà Nội</option>
+                        <option value="Hồ Chí Minh">Hồ Chí Minh</option>
+                        <option value="Huế">Huế</option>
+                        <option value="Nha Trang">Nha Trang</option>
+                        <option value="Hải Dương">Hải Dương</option>
+                        <option value="Quảng Bình">Quảng Bình</option>
+                    </select>
+                    <span><i class="fa-sharp fa-solid fa-location-dot"></i></span>
+                    <br>
+                </form>
+            </div>
+        </div>
+    </section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
