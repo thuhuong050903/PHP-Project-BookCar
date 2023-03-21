59ba465965556d49c713274714fd569da6b24774
PhanAnhThu112 <thu.phan24@student.passerellesnumeriques.org>
2023-03-21 18:21:22 +0700

    Code xong ss 6 - Feed back

---
+++ pages/user/show.php
@@ -0,0 +1,128 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+    </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- <link rel="stylesheet" href="./css/bootstrap.min.css"> -->
+    <!-- <link rel="stylesheet" href="./js/bootstrap.min.js"> -->
+    <!-- <link rel="stylesheet" href="./js/jquery-3.6.1.min.js"> -->
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="container">
+        <section class="sc6">	
+            <div class="container">
+             <div class="text-title1">
+                 <h3>KHÁCH HÀNG NÓI GÌ</h3>
+             </div>
+             <Div class="text-title2">
+                 <p>Chúng tôi tự hào về chất lượng và dịch vụ của mình</p> 
+             </Div>
+
+             <div class="wrap">
+                 <div class="main-text">
+                     <div class="text-box">
+                         <div class="icon">
+                             <i class="fa-solid fa-quote-left"></i>
+                         </div>
+                         <div class="text"> 
+                             <p>Mình rất thích  đến đây bởi vì phong cách rất chuyên nghiệp.Hơn nữa thức uống ở đây rất ngon, có hương vị rất khác biệt , các vị khách của mình vô cùng thích.
+                             </p>
+                         </div>
+                     </div>
+                     <div class="triangle" ></div>
+                     <div class="avatar">
+                         <img src="./img/a1.jpg" alt="" style="width:50px;height:50px;">
+                     </div>
+                     <div class="name">
+                         <p>Bùi Xuân Hoàn</p>
+                     </div>
+
+                 </div>
+                 <div class="main-text">
+                     <div class="text-box">
+                         <div class="icon">
+                             <i class="fa-solid fa-quote-left"></i>
+                         </div>
+                         <div class="text"> 
+                             <p>Phong cách Galleria vô cùng khác biệt nhưng lại hợp ý mình.Mình và các bạn của mình mỗi khi gặp mặt thì đều đến đây cả.Mong Galleria luôn cho nhiều đồ ăn mới nhé.
+                             </p>
+                         </div>
+                     </div>
+                     <div class="triangle" ></div>
+                     <div class="avatar">
+                         <img src="./img/a2.png" alt="" style="width:50px;height:50px;">
+                     </div>
+                     <div class="name">
+                         <p>Nguyễn Hữu Thắng</p>
+                     </div>
+
+                 </div>
+                 <div class="main-text" style="height: 275px;">
+                     <div class="text-box">
+                         <div class="icon">
+                             <i class="fa-solid fa-quote-left"></i>
+                         </div>
+                         <div class="text" > 
+                             <p>Nhân viên tư vấn tận tình. Nhân viên lấy mẫu lại trẻ đẹp, cẩn thận và chu đáo. Nói chung, anh đánh giá dịch vụ tốt. Anh thấy cần phải quảng bá rộng rãi nhiều hơn để có nhiều người biết đến hơn.
+                             </p>
+                         </div>
+                     </div>
+                     <div class="triangle" ></div>
+                     <div class="avatar">
+                <img src="./img/a3.jpg" alt="" style="width:50px;height:50px;">
+                     </div>
+                     <div class="name">
+                         <p>Nguyễn Văn Biên</p>
+                     </div>
+
+                 </div>
+                 <div class="main-text">
+                     <div class="text-box">
+                         <div class="icon">
+                             <i class="fa-solid fa-quote-left"></i>
+                         </div>
+                         <div class="text"> 
+                             <p>Mình rất thích  đến đây bởi vì phong cách rất chuyên nghiệp.Hơn nữa thức uống ở đây rất ngon, có hương vị rất khác biệt , các vị khách của mình vô cùng thích.
+                             </p>
+                         </div>
+                     </div>
+                     <div class="triangle" ></div>
+                     <div class="avatar">
+                         <img src="./img/a4.jpg" alt="" style="width:50px;height:50px;">
+                     </div>
+                     <div class="name">
+                         <p>Phan Đức Thơ</p>
+                     </div>
+                 </div>
+                 <div class="main-text" style="height: 275px;">
+                     <div class="text-box">
+                         <div class="icon">
+                             <i class="fa-solid fa-quote-left"></i>
+                         </div>
+                         <div class="text"> 
+                             <p>Nhân viên tư vấn tận tình. Nhân viên lấy mẫu lại trẻ đẹp, cẩn thận và chu đáo. Nói chung, anh đánh giá dịch vụ tốt. Anh thấy cần phải quảng bá rộng rãi nhiều hơn để có nhiều người biết đến hơn.
+                             </p>
+                         </div>
+                     </div>
+                     <div class="triangle" ></div>
+                     <div class="avatar">
+                         <img src="./img/a5.jpg" alt="" style="width:50px;height:50px;">
+                     </div>
+                     <div class="name">
+                         <p>Phan Thanh Lực</p>
+                     </div>
+
+                 </div>
+            </div>
+
+         </div>
+</body>
+
+</html>
+
