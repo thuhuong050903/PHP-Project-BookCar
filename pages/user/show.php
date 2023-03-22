bce585f65cdfff41b11619b198f442e0420572e0
thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-03-22 17:23:04 +0700

    Update show.php

--- pages/user/show.php
+++ pages/user/show.php
@@ -103,112 +103,111 @@
 <!-- Phan feedback tu khach hang -->
    
 <section class="sc6">	
-            <div class="container">
-             <div class="text-title1">
-                 <h3>KHÁCH HÀNG NÓI GÌ</h3>
-             </div>
-             <Div class="text-title2">
-                 <p>Chúng tôi tự hào về chất lượng và dịch vụ của mình</p> 
-             </Div>
+    <div class="container">
+        <div class="text-title1">
+            <h3>KHÁCH HÀNG NÓI GÌ</h3>
+        </div>
+        <div class="text-title2">
+            <p>Chúng tôi tự hào về chất lượng và dịch vụ của mình</p> 
+        </div>
 
-             <div class="wrap">
-                 <div class="main-text">
-                     <div class="text-box">
-                         <div class="icon">
-                             <i class="fa-solid fa-quote-left"></i>
-                         </div>
-                         <div class="text"> 
-                             <p>Có lần mình chuyển khoản nhầm mua thừa 1 vé, sau đó liên hệ lại với chăm sóc khách hàng thì được hỗ trợ kiểm tra và chuyển khoản lại ngay, không bị thủ tục rườm rà như bên khác. 
-                             </p>
-                         </div>
-                     </div>
-                     <div class="triangle" ></div>
-                     <div class="avatar">
-                         <img src="./images/a1.jpg" alt="" style="width:50px;height:50px;">
-                     </div>
-                     <div class="name">
-                         <p>Bùi Xuân Hoàn</p>
-                     </div>
+        <div class="wrap">
+            <div class="main-text">
+                <div class="text-box">
+                    <div class="icon">
+                        <i class="fa-solid fa-quote-left"></i>
+                    </div>
+                    <div class="text"> 
+                        <p>Có lần mình chuyển khoản nhầm mua thừa 1 vé, sau đó liên hệ lại với chăm sóc khách hàng thì được hỗ trợ kiểm tra và chuyển khoản lại ngay, không bị thủ tục rườm rà như bên khác. 
+                        </p>
+                    </div>
+                </div>
+                <div class="triangle" ></div>
+                <div class="avatar">
+                    <img src="./images/a1.jpg" alt="" style="width:50px;height:50px;">
+                </div>
+                <div class="name">
+                    <p>Bùi Xuân Hoàn</p>
+                </div>
 
-                 </div>
-                 <div class="main-text">
-                     <div class="text-box">
-                         <div class="icon">
-                             <i class="fa-solid fa-quote-left"></i>
-                         </div>
-                         <div class="text"> 
-                             <p>Mình rất hài lòng về dịch vụ của xe Điện Biên. nhân viên lịch sự chu đáo, lái xe cẩn thận không phóng nhanh vượt ẩu, dừng nghỉ đúng chỗ. Tôi cảm thấy hài lòng về dịch vụ xe.
-                             </p>
-                         </div>
-                     </div>
-                     <div class="triangle" ></div>
-                     <div class="avatar">
-                         <img src="./images/a2.png" alt="" style="width:50px;height:50px;">
-                     </div>
-                     <div class="name">
-                         <p>Nguyễn Hữu Thắng</p>
-                     </div>
-
-                 </div>
-                 <div class="main-text" style="height: 275px;">
-                     <div class="text-box">
-                         <div class="icon">
-                             <i class="fa-solid fa-quote-left"></i>
-                         </div>
-                         <div class="text" > 
-                             <p>Hôm qua, mình khá thích thú và hài lòng khi sử dụng dịch vụ xe khoang giường Hà Nội - Sơn La, lái xe nhiệt tình, bạn tiếp viên hỗ trợ mình xách hành lí.
-                             </p>
-                         </div>
-                             </p>
-                     </div>
-                     <div class="triangle" ></div>
-                     <div class="avatar">
-                <img src="./images/a3.jpg" alt="" style="width:50px;height:50px;">
-                     </div>
-                     <div class="name">
-                         <p>Nguyễn Văn Biên</p>
-                     </div>
-
-                 </div>
-                 <div class="main-text">
-                     <div class="text-box">
-                         <div class="icon">
-                             <i class="fa-solid fa-quote-left"></i>
-                         </div>
-                         <div class="text"> 
-                             <p>Xe chạy êm, không phóng nhanh vượt ẩu. Tài xế rất lịch sử, tổng đài trả lời dễ thương. Nhà xe nên mở thêm nhiều tuyến mới để khách hàng có thêm sự lựa chọn.
-                             </p>
-                         </div>
-                     </div>
-                     <div class="triangle" ></div>
-                     <div class="avatar">
-                         <img src="./images/a4.jpg" alt="" style="width:50px;height:50px;">
-                     </div>
-                     <div class="name">
-                         <p>Phan Đức Thơ</p>
-                     </div>
-                 </div>
-                 <div class="main-text" style="height: 275px;">
-                     <div class="text-box">
-                         <div class="icon">
-                             <i class="fa-solid fa-quote-left"></i>
-                         </div>
-                         <div class="text"> 
-                             <p>Đang lễ tết nên cực kì bất ngờ khi nhà xe không nhét khách, chỉ đủ số ghế là đã đi rồi. Phục vụ thì cực kì nhiệt tình, con mình rơi gấu bông ra khỏi túi mà gọi cho anh phụ xe tìm hộ gửi lại văn phòng cho mình.
-                             </p>
-                         </div>
-                     </div>
-                     <div class="triangle" ></div>
-                     <div class="avatar">
-                         <img src="./images/a5.jpg" alt="" style="width:50px;height:50px;">
-                     </div>
-                     <div class="name">
-                         <p>Phan Thanh Lực</p>
-                     </div>
-
-                 </div>
             </div>
-        </section>
+            <div class="main-text">
+                <div class="text-box">
+                    <div class="icon">
+                        <i class="fa-solid fa-quote-left"></i>
+                    </div>
+                    <div class="text"> 
+                        <p>Mình rất hài lòng về dịch vụ của xe Điện Biên. nhân viên lịch sự chu đáo, lái xe cẩn thận không phóng nhanh vượt ẩu, dừng nghỉ đúng chỗ. Tôi cảm thấy hài lòng về dịch vụ xe.
+                        </p>
+                    </div>
+                </div>
+                <div class="triangle" ></div>
+                <div class="avatar">
+                    <img src="./images/a2.png" alt="" style="width:50px;height:50px;">
+                </div>
+                <div class="name">
+                    <p>Nguyễn Hữu Thắng</p>
+                </div>
+
+            </div>
+            <div class="main-text" style="height: 275px;">
+                <div class="text-box">
+                    <div class="icon">
+                        <i class="fa-solid fa-quote-left"></i>
+                    </div>
+                    <div class="text" > 
+                        <p>Hôm qua, mình khá thích thú và hài lòng khi sử dụng dịch vụ xe khoang giường Hà Nội - Sơn La, lái xe nhiệt tình, bạn tiếp viên hỗ trợ mình xách hành lí.
+                        </p>
+                    </div>
+                        
+                </div>
+                <div class="triangle" ></div>
+                <div class="avatar">
+                    <img src="./images/a3.jpg" alt="" style="width:50px;height:50px;">
+                </div>
+                <div class="name">
+                    <p>Nguyễn Văn Biên</p>
+                </div>
+            </div>
+            <div class="main-text">
+                <div class="text-box">
+                    <div class="icon">
+                        <i class="fa-solid fa-quote-left"></i>
+                    </div>
+                    <div class="text"> 
+                        <p>Xe chạy êm, không phóng nhanh vượt ẩu. Tài xế rất lịch sử, tổng đài trả lời dễ thương. Nhà xe nên mở thêm nhiều tuyến mới để khách hàng có thêm sự lựa chọn.
+                        </p>
+                    </div>
+                </div>
+                <div class="triangle" ></div>
+                <div class="avatar">
+                    <img src="./images/a4.jpg" alt="" style="width:50px;height:50px;">
+                </div>
+                <div class="name">
+                    <p>Phan Đức Thơ</p>
+                </div>
+            </div>
+            <div class="main-text" style="height: 275px;">
+                <div class="text-box">
+                    <div class="icon">
+                        <i class="fa-solid fa-quote-left"></i>
+                    </div>
+                    <div class="text"> 
+                        <p>Đang lễ tết nên cực kì bất ngờ khi nhà xe không nhét khách, chỉ đủ số ghế là đã đi rồi. Phục vụ thì cực kì nhiệt tình, con mình rơi gấu bông ra khỏi túi mà gọi cho anh phụ xe tìm hộ gửi lại văn phòng cho mình.
+                        </p>
+                    </div>
+                </div>
+                <div class="triangle" ></div>
+                <div class="avatar">
+                    <img src="./images/a5.jpg" alt="" style="width:50px;height:50px;">
+                </div>
+                <div class="name">
+                    <p>Phan Thanh Lực</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
  
 
     <!-- ------------------------------------------ -->
