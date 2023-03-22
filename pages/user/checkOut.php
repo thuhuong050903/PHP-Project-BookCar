c3a283f08ebb716fb40073bded0d9915b58aa410
thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-03-22 13:35:03 +0700

    include file

--- pages/user/checkOut.php
+++ pages/user/checkOut.php
@@ -12,6 +12,19 @@
         include "/pages/components/header.php";
         ?>
     </header>
-    
+    <form class="form-information" method="post">
+        <div class="form-information-group">
+            <div class="tiltle">Name <span class="required">*</span></div>
+            <input type="text" name="name" value="" placeholder="Please enter your name">
+        </div>
+        <div class="group">
+            <div class="tiltle">Phone number <span class="required">*</span></div>
+            <input type="text" name="phoneNumber" value="" placeholder="Please enter your phone number">
+        </div>
+        <div class="group">
+            <div class="tiltle">Email to receive trip information <span class="required">*</span></div>
+            <input type="text" name="email" value="" placeholder="Please enter your email">
+        </div>
+    </form>
 </body>
 </html>
