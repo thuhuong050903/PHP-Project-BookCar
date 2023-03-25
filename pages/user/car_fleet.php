c549208ad67ecf143d7d4a29bf21a488877f3223
thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-03-25 10:14:28 +0700

    Tạo form ss1

---
+++ pages/user/car_fleet.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Cars Fleet</title>
+</head>
+<body>
+    <section class="sectionChooseFleet">
+        <div class="sectionChooseFleet-title">
+            <form action="" method="post" class="sectionChooseFleet-form">
+                <select name="form-car-make" id="form-car-make">
+                    <option value="none">Choose a car make</option>  <?php // echo $_POST[""] ?>
+                    <option value="Audi">Audi</option>
+                    <option value="BMW">BMW</option>
+                    <option value="Ford">Mercedes</option>
+                    <option value="Mazda">Mazda</option>
+                    <option value="Mercedes">Mercedes</option>
+                </select>
+                <select name="form-car-type" id="form-car-type">
+                    <option value="none">Choose a car type</option>
+                    <option value="Hatchback">Hatchback</option>
+                    <option value="Luxury">Luxury</option>
+                    <option value="Ford">Mercedes</option>
+                    <option value="Mazda">Mazda</option>
+                    <option value="Mercedes">Mercedes</option>
+                </select>
+                <input type="" name="" value="">
+                <input type="" name="" value="">
+            </form>
+        </div>
+    </section>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
+</body>
+</html>
