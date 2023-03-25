67036ced57132c1e2d27928c87dbbb5cf42a2997
thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-03-25 12:01:47 +0700

    code xong ss1

--- pages/user/car_fleet.php
+++ pages/user/car_fleet.php
@@ -14,24 +14,32 @@
     <section class="sectionChooseFleet">
         <div class="sectionChooseFleet-title">
             <form action="" method="post" class="sectionChooseFleet-form">
+                <?php
+                    include('data_car_fleet.php'); 
+                    for ($i=0;$i <= sizeof($cars_make);$i++) {  
+                ?>
                 <select name="form-car-make" id="form-car-make">
-                    <option value="none">Choose a car make</option>  <?php // echo $_POST[""] ?>
-                    <option value="Audi">Audi</option>
-                    <option value="BMW">BMW</option>
-                    <option value="Ford">Mercedes</option>
-                    <option value="Mazda">Mazda</option>
-                    <option value="Mercedes">Mercedes</option>
-                </select>
+                    <option value="none">Choose a car make</option>  
+                    <option value="<?php echo $cars_make[0]?>"><?php echo $cars_make[0] ?></option>
+                    <option value="<?php echo $cars_make[1] ?>"><?php echo $cars_make[1] ?></option>
+                    <option value="<?php echo $cars_make[2] ?>"><?php echo $cars_make[2] ?></option>
+                    <option value="<?php echo $cars_make[3] ?>"><?php echo $cars_make[3] ?></option>
+                    <option value="<?php echo $cars_make[4] ?>"><?php echo $cars_make[4] ?></option>
+                </select> 
+                <?php } 
+                    for($i = 1; $i <= sizeof($cars_type); $i++) {
+                        ?>
                 <select name="form-car-type" id="form-car-type">
                     <option value="none">Choose a car type</option>
-                    <option value="Hatchback">Hatchback</option>
-                    <option value="Luxury">Luxury</option>
-                    <option value="Ford">Mercedes</option>
-                    <option value="Mazda">Mazda</option>
-                    <option value="Mercedes">Mercedes</option>
+                    <option value="<?php echo $cars_type[0] ?>"><?php echo $cars_type[0] ?></option>
+                    <option value="<?php echo $cars_type[1] ?>"><?php echo $cars_type[1] ?></option>
+                    <option value="<?php echo $cars_type[2] ?>"><?php echo $cars_type[2] ?></option>
+                    <option value="<?php echo $cars_type[3] ?>"><?php echo $cars_type[3] ?></option>
+                    <option value="<?php echo $cars_type[4] ?>"><?php echo $cars_type[4] ?></option>
                 </select>
-                <input type="" name="" value="">
-                <input type="" name="" value="">
+                <?php } ?>
+                <input type="text" name="search" value="" placeholder="Search">
+                <input type="submit" name="submit" value="Search" >
             </form>
         </div>
     </section>
---
+++ pages/user/data_car_fleet.php
@@ -0,0 +1,5 @@
+<?php 
+    $cars_make = ['Audi','BMW','Ford','Mercedes','Mazda'];
+    $car_type = ['Hatchback','Luxury','MPV','Sedan','SUV'];
+
+?>
