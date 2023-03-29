37671a7425b0e2c304e85fb692512c10859db859
thuhuong050903 <huong.phan24@student.passerellesnumeriques.org>
2023-03-29 18:34:11 +0700

    tao database

--- pages/user/connect.php
+++ pages/user/connect.php
@@ -1,3 +1,7 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","product")
+    $conn = mysqli_connect("localhost","root","");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+      echo "Connected successfully";
 ?>
